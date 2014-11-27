9824392717490d0fb6e16c211e4da420e5177988
João Justo <jpjustonunes@hotmail.com>
2014-11-27 11:28:06 +0000

    fecths open pr's from given github repo and saves them in a collection

--- kraken.js
+++ kraken.js
@@ -1,23 +1,69 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
+PullRequests = new Mongo.Collection("pullRequests");
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
+if (Meteor.isClient) {
+  Template.getPullRequests.events({
+    'submit': function (event) {
+      event.preventDefault();
+      var org = $('#org').val();
+      var repo = $('#repo').val();
+      Meteor.call('getPRs', org, repo, function (error, result) {
+        if (error)
+          console.log(error);
+        else
+          console.log(result);
+          result.result.forEach(function(pr){
+            console.log(pr.title);
+            Meteor.call('addPRs', pr.title);
+          });
+      });
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
+  Template.getPullRequests.helpers({
+    pullRequests: function () {
+      var prs = PullRequests.find().fetch();
+      console.log(prs.title);
+      return prs;
     }
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    Meteor.methods({
+      'addPRs': function addPRs (title) {
+        PullRequests.insert({
+          title: title
+        });
+      },
+
+      'getPRs': function getPRs (org, repo) {
+        var GithubApi = Meteor.npmRequire('github');
+        var github = new GithubApi({
+          version: "3.0.0",
+          // optional
+          debug: true,
+          protocol: "https",
+          pathPrefix: "/api/v3", // for some GHEs
+          timeout: 5000,
+          headers: {
+            "user-agent": "My-Cool-GitHub-App", // GitHub is happy with a unique user agent
+          }
+        });
+        var prs = Async.runSync(function(done) {
+          github.pullRequests.getAll({
+            // optional:
+            // headers: {
+            //     "cookie": "blahblah"
+            // },
+            user: org,
+            repo: repo
+          }, function(err, res) {
+            done(null, res);
+          });
+        });
+        return prs;
+      }
+    });
   });
 }
